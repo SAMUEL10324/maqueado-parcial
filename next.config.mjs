f587340cc062c7118cadb2cf2e0e4442e1113828
Caceres Samuel <aridan045@gmail.com>
2026-06-01 16:14:02 -0300

    Despliegue 2

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
 }
 
-export default nextConfig
+export default nextConfig;
